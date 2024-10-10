d31bd9553f470405154dff57a1b4fe0b14a609ea
GantaSaikanth <154391801+GantaSaikanth@users.noreply.github.com>
2024-10-10 13:34:50 +0530

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,11 @@ import {IoCartOutline} from 'react-icons/io5'
 import './index.css'
 
 const Header = props => {
-  const {count} = props
+  const {count, cartDataName} = props
 
   return (
     <div className="header-container">
-      <h1>UNI Resto Cafe</h1>
+      <h1>{cartDataName}</h1>
       <div className="order-container">
         <p className="order-para">My Orders</p>
         <div className="cart-container">
